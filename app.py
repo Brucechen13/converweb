79c1924a86dd64f3feff8fcdbc1196b7ed41afe0
Brucechen13 <chenchi_94@163.com>
2017-04-26 16:06:42 +0800

    complete books

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,url_for
 from flask.ext.bootstrap import Bootstrap
 from movieSpide import getMovie
 from musicSpide import getMusic
+from novieSpide import getBooks
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -24,7 +25,12 @@ def movies():
 def musics():
     return render_template('music.html',musics = musics)
 
+@app.route('/books')
+def books():
+    return render_template('books.html',books = books)
+
 if __name__ == '__main__':
     movies = getMovie() 
     musics = getMusic() 
-    app.run(port=5002,debug=True)
+    books = getBooks()
+    app.run(port=5002,debug=True) 
--- novieSpide.py
+++ novieSpide.py
@@ -2,47 +2,48 @@ import requests
 import threading
 from lxml import etree
 
-basicUrl = ['http://www.23us.com/html/51/51514/','http://www.23us.com/html/24/24665/','http://www.23us.com/html/49/49940/']
+basicUrl = "https://www.amazon.cn"
+url = ['https://www.amazon.cn/gp/new-releases/books/ref=sv_b_2']
 
-def saveTxt(url):
+def getBooks():
+    return get_html(url[0])
+
+def get_html(url):
     headers = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
-        'Host':'www.23us.com',
-        'Referer':'http://www.23us.com/book/51514',
+        'Host':'www.amazon.cn',
+        'Connection':'keep-alive',
+        'Referer':'https://www.amazon.cn/gp/new-releases/books/ref=sv_b_2',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
     }
+    data = []
     res = requests.get(url, headers= headers)
-    res.encoding = 'gbk'
     doc = etree.HTML(res.text)
-    bodyText = doc.xpath('//div[@class = "bdsub"]/dl')[0]
-    title = bodyText.xpath('./dd/h1/text()')[0].split(' ')[0]
-    #print(title)
-    book = open(title+'.txt', 'w', encoding='utf-8')
-    book.write(title+"\n")
-    chapters = doc.xpath('//td[@class="L"]/a')
-    for i in range(0, min(100, len(chapters))):
-        chapter = chapters[i]
-        chapterTxt = chapter.xpath('./text()')[0]
-        if (chapterTxt.find('章') != -1) and (chapterTxt.find("月票") == -1):
-            #print(chapterTxt)
-            book.write(chapterTxt+"\n")
-            chapterUrl = chapter.xpath('./@href')[0]
-            res_detail = requests.get(url+chapterUrl, headers= headers)
-            res_detail.encoding = 'gbk'
-            doc_detail = etree.HTML(res_detail.text)
-            doc_detail = doc_detail.xpath('//dd[@id="contents"]')[0]
-            contents = doc_detail.xpath('./text()')
-            chapterTxt = ""
-            for content in contents:
-                chapterTxt += content
-                chapterTxt += '\n'
-            chapterTxt += '\n'            
-            book.write(chapterTxt)
-    book.close()
+    books = doc.xpath('//div[@class = "a-fixed-left-grid-col a-col-right"]')
+    #print(len(books))
+    for i in range(0, min(10, len(books))):
+        content = books[i]
+        book = {}
+        book['url'] = basicUrl+content.xpath('./a/@href')[0]
+        book['title'] = content.xpath('./a/div/text()')[0].strip()
+        #print(book['title'])
+        book['auther'] = content.xpath('./div[@class="a-row a-size-small"]/span/text()')[0]
+        book_detail = etree.HTML(requests.get(book['url'], headers=headers).text)
+        book['picUrl'] = book_detail.xpath('//div[@id="img-canvas"]/img/@data-a-dynamic-image')[0].split('":')[0][2:]
+        book['intro'] = book_detail.xpath('//noscript/div/text()')[0]
+        book_comments = book_detail.xpath('//div[@id="revMHRL"]/div[@class="a-section celwidget"]/div[@class="a-row a-spacing-micro"]')
+        book['comments'] = []
+        for comment in book_comments:
+            commentDict = {}
+            commentDict['msg'] = comment.xpath('./div/a')[1].xpath('./span/text()')[0]
+            commentDict['auth'] = comment.xpath('./span/span[@class="a-size-normal"]/a/text()')[0]
+            book['comments'].append(commentDict)
+        data.append(book)
+    return data
+
+#print(get_html(url[0]))
     
-for i in range(len(basicUrl)):
-    threading.Thread(target = saveTxt, args = (basicUrl[i], )).start()
     
     
